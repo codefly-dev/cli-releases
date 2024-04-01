217f74501e7c9954f2ce1e783c85449556655fdc
Antoine Toussaint <antoine@antoines-mbp-2.myfiosgateway.com>
2024-04-01 12:33:43 -0400

    fix: install script

--- install/linux.sh
+++ install/linux.sh
@@ -1,21 +1,38 @@
 #!/bin/bash
-
+ 
 # GitHub user/repo
 USER_REPO="codefly-dev/cli-releases"
-
+ 
 # Fetch the latest release data from GitHub API
 URL="https://github.com/$USER_REPO/releases/latest"
-
+ 
 # Use curl to fetch the latest release page and grep to parse the tag
 TAG=$(curl -sL $URL | grep -oP 'href="/'$USER_REPO'/releases/tag/\K[^"]+' | head -1)
-
+ 
 # Construct the download URL
-# Assuming the naming convention is 'codefly_<tag>_Linux_64-bit.tar.gz'
+# Assuming the naming convention is 'codefly_<tag>_linux-x86_64-bit.tar.gz'
 # Adjust the naming convention based on your actual release asset names
-FILENAME="codefly_${TAG}_Linux_64-bit.tar.gz"
+FILENAME="codefly_linux_64-bit.tar.gz"
 DOWNLOAD_URL="https://github.com/$USER_REPO/releases/download/$TAG/$FILENAME"
-
-# Download the file
+ 
+# Download the file                                                                                                                                                                                                  
 curl -L $DOWNLOAD_URL -o $FILENAME
 
-echo "Downloaded $FILENAME"
+# Untar the file
+tar -xzf $FILENAME
+
+# Assuming the binary is named 'codefly'
+# Adjust the binary name based on your actual binary name
+BINARY_NAME="codefly"
+
+# Move the binary to /usr/local/bin
+sudo mv $BINARY_NAME /usr/local/bin
+
+# Make the binary executable
+sudo chmod +x /usr/local/bin/$BINARY_NAME
+
+# Clean up the downloaded tar file
+rm $FILENAME
+
+echo "Installation completed successfully."
+
