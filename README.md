b80bcc5581c5be6360afb0cc20604037f99aaa56
Antoine Toussaint <antoine@antoines-mbp-2.myfiosgateway.com>
2024-04-08 07:35:58 -0400

    fix: install script

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-R
+# Installation
+
+## Mac
+
+```shell
+brew tap codefly-dev/cli
+brew install codefly
+```
+
+## Linux
+
+```shell
+curl -s https://raw.githubusercontent.com/codefly-dev/cli-releases/main/install/linux.sh | bash
+```
--- install/linux.sh
+++ install/linux.sh
@@ -12,27 +12,28 @@ TAG=$(curl -sL $URL | grep -oP 'href="/'$USER_REPO'/releases/tag/\K[^"]+' | head
 # Construct the download URL
 # Assuming the naming convention is 'codefly_<tag>_linux-x86_64-bit.tar.gz'
 # Adjust the naming convention based on your actual release asset names
-FILENAME="codefly_linux_64-bit.tar.gz"
+FILENAME="codefly-linux-x86_64"
 DOWNLOAD_URL="https://github.com/$USER_REPO/releases/download/$TAG/$FILENAME"
- 
+
+echo "Downloading $DOWNLOAD_URL"
+
 # Download the file                                                                                                                                                                                                  
-curl -L $DOWNLOAD_URL -o $FILENAME
+curl -f -L $DOWNLOAD_URL -o $FILENAME -#
 
-# Untar the file
-tar -xzf $FILENAME
+# Check if the file was downloaded correctly
+if [ $? -eq 0 ]; then
 
-# Assuming the binary is named 'codefly'
-# Adjust the binary name based on your actual binary name
-BINARY_NAME="codefly"
+    # Assuming the binary is named 'codefly'
+    # Adjust the binary name based on your actual binary name
+    BINARY_NAME="codefly"
 
-# Move the binary to /usr/local/bin
-sudo mv $BINARY_NAME /usr/local/bin
+    # Move the binary to /usr/local/bin
+    mv $FILENAME /usr/local/bin/$BINARY_NAME
 
-# Make the binary executable
-sudo chmod +x /usr/local/bin/$BINARY_NAME
+    # Make the binary executable
+    chmod +x /usr/local/bin/$BINARY_NAME
 
-# Clean up the downloaded tar file
-rm $FILENAME
-
-echo "Installation completed successfully."
-
+    echo "Installation completed successfully."
+else
+    echo "File download failed. Please check the download URL."
+fi
